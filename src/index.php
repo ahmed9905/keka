dfc3773946fcee69a70ff8974db7e092298d7f99
Ahma <ahmed151p@mail.ru>
2018-03-28 19:52:30 +0300

    +index

--- src/index.php
+++ src/index.php
@@ -10,20 +10,37 @@ require_once 'login.php';
 	}
 
 ?>
-<!DOCTYPE>
-<html>
-<head>
-<title>master</title>
-</head>
-<body>
-	<form method="POST">
-	Логин <input name="login" type="text" required><br>
-	Пароль <input name="password" type="password" required><br>
-	<input name="aut" type="submit" value="Войти">
-    </form>
-	<?
-	echo $errLogin;
-  	?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://fonts.googleapis.com/css?family=Amatic+SC" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/bootstrap.css">
+    
+  </head>
+  <body>
+	 <form method="POST">
+          <div class="intro">
+            <div class="intro1">
+              <div class="text-main">
+                <h1>Мой дневник</h1>
+              </div>
+              <div class="vvod1">
+                <p>Ваш логин</p>
+                <input size="25"  name="login" type="text" required">
+                <p>Ваш пароль</p>
+                <input size="25" name="password" type="password" required">
+              </div>
+              <div class="butt">
+                     <input name ="aut" type="submit" value="Войти">
+            </div>
+            </div>
+          </div>
+          </form>
 </body>
 </html>
 
